68f473f8fa3c9c4f7a2cafd25b71a1179c4b1fb6
Kipyegon Wycliffe Langat <KLANGAT@safaricom.co.ke>
2021-12-06 07:23:28 +0300

    introducing a function for getting date

--- assigment.js
+++ assigment.js
@@ -1,2 +1,5 @@
-// var name =prompt("What is your name");
-// console.log(name);
+var name =prompt("What is your name");
+console.log(name);
+
+function week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+
