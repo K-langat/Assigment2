8833d4526adfcdc980d4aa8419307db665ef5b55
Kipyegon Wycliffe Langat <KLANGAT@safaricom.co.ke>
2021-12-06 08:19:01 +0300

    adding variable for male name

--- assigment.js
+++ assigment.js
@@ -3,3 +3,12 @@ console.log(name);
 
 function week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
 
+var nameMale = weekdate
+sunday = "Kwasi";
+monday = " Kwabena";
+wednesday = " Kwaku"
+Thursday = "Yaw";
+Friday ="Kofi"
+Saturday ="Kwame"
+
+
